438b8047f19e3475054ab485cadf29317a49d256
Adam O'Connor <aoconnor@squaremouth.com>
2014-02-07 09:32:00 -0500

    Load has_breadcrumb at runtime
    This commit removes the need for the has_breadcrumb initializer by
    instead loading up the has_breadcrumb modules when the gem is loaded.

--- lib/has_breadcrumb.rb
+++ lib/has_breadcrumb.rb
@@ -2,3 +2,5 @@ require "has_breadcrumb/version"
 require "has_breadcrumb/has_breadcrumb.rb"
 require "has_breadcrumb/show_breadcrumb.rb"
 
+ActiveRecord::Base.class_eval { include HasBreadcrumb }
+ActionView::Base.class_eval { include ShowBreadcrumb }
