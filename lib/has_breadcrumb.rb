2cb0cd55edb96ee6f593ca4f3fa2d9e0d3cbb56e
Adam O'Connor <aoconnor@squaremouth.com>
2014-02-07 11:57:53 -0500

    Place initializer code into railtie class and edited README
    This commit moves the initialization of the has_breadcrumbs modules into
    a railtie class which removes the necessity of using an initializer in
    the hosting application.
    
    Also updated was cleaning up a few sentences in the README.

--- lib/has_breadcrumb.rb
+++ lib/has_breadcrumb.rb
@@ -1,6 +1,4 @@
 require "has_breadcrumb/version"
 require "has_breadcrumb/has_breadcrumb.rb"
 require "has_breadcrumb/show_breadcrumb.rb"
-
-ActiveRecord::Base.class_eval { include HasBreadcrumb }
-ActionView::Base.class_eval { include ShowBreadcrumb }
+require "has_breadcrumb/railtie" if defined?(Rails)
